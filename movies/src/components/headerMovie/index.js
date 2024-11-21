bd0110eb0e005b16306d768cc46f331b4d85c1ac
michaelogunn <mogunnaike96@gmail.com>
2024-11-21 17:19:42 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/components/headerMovie/index.js
+++ movies/src/components/headerMovie/index.js
@@ -1,75 +1,42 @@
 import React from "react";
-import Chip from "@mui/material/Chip";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import MonetizationIcon from "@mui/icons-material/MonetizationOn";
-import StarRate from "@mui/icons-material/StarRate";
-import NavigationIcon from "@mui/icons-material/Navigation";
-import Fab from "@mui/material/Fab";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import HomeIcon from "@mui/icons-material/Home";
 
-
-const root = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: 1.5,
-    margin: 0,
-};
-const chip = { margin: 0.5 };
-
-const MovieDetails = ( props) => {
-  const movie = props.movie
+const MovieHeader = (props) => {
+  const movie = props.movie;
 
   return (
-    <>
-      <Typography variant="h5" component="h3">
-        Overview
-      </Typography>
-
-      <Typography variant="h6" component="p">
-        {movie.overview}
-      </Typography>
-
-      <Paper 
-        component="ul" 
-        sx={{...root}}
-      >
-        <li>
-          <Chip label="Genres" sx={{...chip}} color="primary" />
-        </li>
-        {movie.genres.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} sx={{...chip}} />
-          </li>
-        ))}
-      </Paper>
-      <Paper component="ul" sx={{...root}}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count})`}
-        />
-        <Chip label={`Released: ${movie.release_date}`} />
-      </Paper>
-      <Fab
-        color="secondary"
-        variant="extended"
+    <Paper 
+        component="div" 
         sx={{
-            position: "fixed",
-            bottom: 2,
-            right: 2
+            display: "flex",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            padding: 1.5,
+            margin: 0,
         }}
       >
-        <NavigationIcon />
-        Reviews
-      </Fab>
-      </>
+      <IconButton aria-label="go back">
+        <ArrowBackIcon color="primary" fontSize="large" />
+      </IconButton>
+
+      <Typography variant="h4" component="h3">
+        {movie.title}
+        <a href={movie.homepage}>
+          <HomeIcon color="primary" />
+        </a>
+        <br />
+        <span sx={{ fontSize: "1.5rem" }}>{`   "${movie.tagline}"`} </span>
+      </Typography>
+      <IconButton aria-label="go forward">
+        <ArrowForwardIcon color="primary" fontSize="large" />
+      </IconButton>
+    </Paper>
   );
 };
-export default MovieDetails ;
+
+export default MovieHeader;
--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -12,44 +12,65 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from '../../images/film-poster-placeholder.png'
+import { Link } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
 
 export default function MovieCard(props) {
-  const movie = props.movie;
-  return (
-    <Card>
-      <CardHeader title={movie.title} sx={{ textWrap: "nowrap"}}/>
-      <CardMedia
-        sx={{ height: 500 }}
-        image={
-          movie.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-            : img
-        }
-      />
-      <CardContent>
-        <Grid container>
-          <Grid size={{xs: 6}}>
-            <Typography variant="h6" component="p">
-              <CalendarIcon fontSize="small" />
-              {movie.release_date}
-            </Typography>
-          </Grid>
-          <Grid size={{xs: 6}}>
-            <Typography variant="h6" component="p">
-              <StarRateIcon fontSize="small" />
-              {"  "} {movie.vote_average}{" "}
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" onClick={null}>
-          <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    const movie = props.movie;
+    const handleAddToFavorite = (e) => {
+        e.preventDefault();
+        props.selectFavorite(movie.id);
+    };
+    return (
+        <Card>
+            <CardHeader
+                avatar={
+                    movie.favorite ? (
+                        <Avatar sx={{ backgroundColor: 'red' }}>
+                            <FavoriteIcon />
+                        </Avatar>
+                    ) : null
+                }
+                title={
+                    <Typography variant="h5" component="p">
+                        {movie.title}{" "}
+                    </Typography>
+                }
+            />
+            <CardMedia
+                sx={{ height: 500 }}
+                image={
+                    movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                        : img
+                }
+            />
+            <CardContent>
+                <Grid container>
+                    <Grid size={{ xs: 6 }}>
+                        <Typography variant="h6" component="p">
+                            <CalendarIcon fontSize="small" />
+                            {movie.release_date}
+                        </Typography>
+                    </Grid>
+                    <Grid size={{ xs: 6 }}>
+                        <Typography variant="h6" component="p">
+                            <StarRateIcon fontSize="small" />
+                            {"  "} {movie.vote_average}{" "}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
+                    <FavoriteIcon color="primary" fontSize="large" />
+                </IconButton>
+                <Link to={`/movies/${movie.id}`}>
+                    <Button variant="outlined" size="medium" color="primary">
+                        More Info ...
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
 }
